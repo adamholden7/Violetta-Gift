5d5115ae2839417538556abbd7717580f96349bd
adamholden7 <168860257+adamholden7@users.noreply.github.com>
2025-03-01 21:52:47 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.querySelector(".card").addEventListener("click", function() {
-    this.classList.toggle("open");
+$(document).ready(function() {
+    var envelope = $("#envelope");
+
+    envelope.click(function() {
+        envelope.toggleClass("open close");
+
+        if (envelope.hasClass("open")) {
+            $(".letter").css("transform", "translateY(-120px)").css("z-index", "5"); // Move letter up
+            $(".flowers").css("opacity", "1"); // Show flowers
+        } else {
+            $(".letter").css("transform", "translateY(0)").css("z-index", "2"); // Reset letter
+            $(".flowers").css("opacity", "0"); // Hide flowers
+        }
+    });
 });
