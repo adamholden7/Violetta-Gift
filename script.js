57134652b00144c91c50007d8326400e4a118d4d
adamholden7 <168860257+adamholden7@users.noreply.github.com>
2025-03-01 22:31:27 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,10 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
             document.querySelector(".letter").style.transform = "translateY(-140px)";
             document.querySelector(".letter").style.zIndex = "5"; 
             document.querySelector(".bouquet").style.opacity = "1"; // Show bouquet
+            
+            // Show hearts
+            document.querySelectorAll(".heart").forEach((heart) => {
+                heart.style.display = "block"; 
+                heart.style.opacity = "1";
+            });
+
         } else {
             document.querySelector(".letter").style.transform = "translateY(0)";
             document.querySelector(".letter").style.zIndex = "2"; 
             document.querySelector(".bouquet").style.opacity = "0"; // Hide bouquet
+            
+            // Hide hearts
+            document.querySelectorAll(".heart").forEach((heart) => {
+                heart.style.display = "none";
+                heart.style.opacity = "0";
+            });
         }
     });
 });
